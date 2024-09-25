97524dc98bde1aa19a5c7b6c1b29890e419381e1
Naveen_Kumar <naveenbighan34@gmail.com>
2024-09-26 00:40:40 +0530

    created several visualizations using matplotlib and seaborn to display common job titles, companies with the most listings, and the most in-demand skills.

--- data_all_csv.py
+++ data_all_csv.py
@@ -4,18 +4,19 @@ import spacy
 from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load("en_core_web_sm")
-# Load data
+
 df = pd.read_csv("all_cleaned.csv")
 pd.options.display.max_rows = 50999
 
-# Title mapping dictionary
+df['Company-Name'] = df['Company-Name'].str.upper()
+
 title_mappings = {
-    r'(?i)mechanical\s+engineer': 'Mechanical Engineering',
-    r'(?i)electrical\s+engineer': 'Electrical Engineering',
-    r'(?i)developer|software|python|application|it|ai/ml|data engineer|cloud|oracle|saas|machine|web|linux|devops|data scientist|Angular|Database|Generative|Computer|Azure|Development|AI|UI UX|data|sql|c++|Technology|Artificial Intelligence|Backend|frontend|ODOO|iOS|android|MongoDB|Internet of Things|iot|java|aws|Firmware|Node.js|express.js|mern|UI/UX': 'Software Developer',
+    r'(?i)mechanical': 'Mechanical Engineering',
+    r'(?i)electrical|electronic': 'Electrical Engineering',
+    r'(?i)developer|software|python|it|ai/ml|cloud|oracle|web|linux|devops|data\s+scientist|angular|database|azure|sql|c\+\+|backend|frontend|ios|android|mongodb|iot|java|aws|node\.js|express\.js|mern|ui/ux': 'Software Developer',
     r'(?i)business': 'Business Development',
-    r'(?i)account manager': 'Account Manager',
-    r'(?i)b2b': 'Sales Manager',
+    r'(?i)account': 'Account Manager',
+    r'(?i)b2b|sales': 'Sales Manager',
     r'(?i)automation': 'Automation Engineering',
     r'(?i)piping': 'Piping Engineering',
     r'(?i)product': 'Product Engineer',
@@ -34,7 +35,6 @@ title_mappings = {
     r'(?i)support': 'Support Engineer',
     r'(?i)ping': 'Ping Engineer',
     r'(?i)principal': 'Principal Engineer',
-    r'(?i)ci/cd': 'Software Developer',
     r'(?i)electronics': 'Electronics Engineer',
     r'(?i)sap': 'Product Manager',
     r'(?i)analytics': 'Software Developer',
@@ -63,7 +63,6 @@ title_mappings = {
     r'(?i)instrumentation': 'Instrumentation Engineer',
 }
 
-# Function to clean the title using the dictionary
 def clean_title(title):
     if isinstance(title, str):
         for pattern, new_title in title_mappings.items():
@@ -71,8 +70,7 @@ def clean_title(title):
                 return new_title
     return title
 
-# Create new columns for original and cleaned titles
-df['original-titles'] = df['Title']
+df['original-titles'] = df['Title'].str.upper()
 df['Title'] = df['original-titles'].apply(clean_title)
 
 df['salary_package'].fillna('N/A', inplace=True)
@@ -81,19 +79,28 @@ df['Timings'] = df['Timings'].replace("Permanent", "Full-Time")
 
 for index, row in df.iterrows():
     if row['original-titles'] != row['Title']:  
-          f"Original: {row['original-titles']} -> Cleaned: {row['Title']}"
+        f" Cleaned: {row['Title']}"
+
 skills_list = [
-    "Python", "Java", "SQL", "JavaScript", "HTML", "CSS", "AWS", "Node.js", 
-    "Docker", "Kubernetes", "Angular", "React", "Machine Learning", "DevOps",
-    "Cloud", "C++", "Linux", "Git", "Agile", "Data Science", "MongoDB"
+    "Python", "Java", "SQL", "JavaScript", "HTML", "CSS", "AWS", "Node.js", "Node", 
+    "Docker", "Kubernetes", "Angular", "React", "Machine Learning", "DevOps", "Web apis",
+    "Cloud", "C++", "Linux", "Git", "Agile", "Data Science", "MongoDB", "Express.js", "Express js",
+    "No SQL", "Springboot", "C#", "OOPS", ".NET", "FastAPI", "PHP", "Django", "Flask", "ASP.NET", "MySQL"," Technical "
+    "Jquery", "Ajax", "Laravel", "GIT", "Git", "Phalcon", "SnowSQL", "Ethernet", "PostgreSQL", "Perl", "Ruby","Product Knowledge",
+    "IoT", "AI", "Github", "Data Analysis", "Digital Marketing", "Communication", "Sales", "Leadership", "Management","SaaS",
+    "Event Planning", "Social Media Marketing", "Media Management", "Content Marketing", "Email Marketing", "Google Ads","Microsoft Office"
+    "Interpersonal skills", "Multitasking", "MS Office", "PowerPoint", "CQF", "PRM", "MS Teams", "SAP", "TypeScript", "Cyber security",
+    "Machine Learning", "Problem solving", "Project Management", "Power BI", "Statistics", "AV programming", "Tensorflow", "NLP",
+    "Proposal Writing","Content Creation","Analytical","Computer","Market Research","B2B marketing","CPQ","LWC Development","Account Management",
+    "Lead Generation","CRM","Negotiation","Presentation"," Market Analysis","Automation","Distribution","Customer Service","MIS Management",
+    "Technical Support","Security domain","Real Estate","Enterprise sales ","Relationship Management","Business Development","Distribution",
+    "Industrial Sales","Cybersecurity","Supply Chain","Journalism","Content writing","Marketing","Creativity"
 ]
 
-# Use PhraseMatcher to find skill-related keywords in the requirements
-matcher = PhraseMatcher(nlp.vocab)
+matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
 patterns = [nlp(skill) for skill in skills_list]
 matcher.add("Skills", None, *patterns)
 
-# Function to extract skills from requirements
 def extract_skills(requirement):
     doc = nlp(requirement)
     matches = matcher(doc)
@@ -103,17 +110,7 @@ def extract_skills(requirement):
         skills_found.add(span.text)
     return ', '.join(skills_found) if skills_found else 'N/A'
 
-# Apply the function to the 'Requirement' column and store in 'skills' column
 df['skills'] = df['Requirement'].apply(lambda x: extract_skills(str(x)))
 
 
-# df.drop(columns=['Requirement'], inplace=True)
-
-
-
-# Print the updated dataframe
-print(df[['skills']].head(50))       
-
-
-
-# df.to_csv("new_csv_title.csv", index=False, encoding='utf-8-sig')
+df.to_csv("new_all_cleaned.csv", index=False, encoding='utf-8-sig')
